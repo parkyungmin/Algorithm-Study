f1d2de7c26d99576cd80013f3f9f80ced7c8b47b
박경민 <pecobee813@naver.com>
2024-03-31 09:39:56 +0900

    [BOJ]A*B/브론즈5/https://www.acmicpc.net/problem/10998

---
+++ gyeongmin/Week_13/BOJ_240330_A_Calculate_B.java
@@ -0,0 +1,19 @@
+package gyeongmin.Week_13;
+
+import java.util.Scanner;
+
+public class BOJ_240330_A_Calculate_B {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
