64230d294b9f01e3e6f37d6dbb3883fd86344366
박경민 <pecobee813@naver.com>
2024-03-31 09:49:00 +0900

    [BOJ]개/브론즈3/https://www.acmicpc.net/problem/10172

---
+++ gyeongmin/Week_13/BOJ_240331_개.java
@@ -0,0 +1,20 @@
+package gyeongmin.Week_13;
+
+public class BOJ_240331_개 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+        /*
+        큰 따옴표 ( " ) 나 백슬래시 ( \ ) 는 단독으로 써서 출력할 수가 없다.
+        그래서 이를 출력하기 위해 특정 구문을 사용하는데 이를 이스케이프라 한다.
+
+        사용법 : 백슬래시(\)와 문자 하나를 결합하여 나타낸다.
+        ex) 백슬래시를 출력하려면 \\로 해줘야 \하나가 출력된다.
+        ex) 두개 출력하고 싶으면 \\\\로 해줘야한다.
+        */
+    }
+}
