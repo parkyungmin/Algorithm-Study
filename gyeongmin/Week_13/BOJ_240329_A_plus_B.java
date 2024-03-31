a03a262ad64f94d5057e22727fdeda485db47a98
박경민 <pecobee813@naver.com>
2024-03-31 09:39:12 +0900

    [BOJ]A+B/브론즈5/https://www.acmicpc.net/step/1

---
+++ gyeongmin/Week_13/BOJ_240329_A_plus_B.java
@@ -0,0 +1,15 @@
+package gyeongmin.Week_13;
+
+import java.util.Scanner;
+
+public class BOJ_240329_A_plus_B {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A, B ;
+
+        A = scanner.nextInt();
+        B = scanner.nextInt();
+        System.out.println(A+B);
+    }
+}
