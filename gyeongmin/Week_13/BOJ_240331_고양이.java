8f413162248d90b577d9f1971cf74c4446da7230
박경민 <pecobee813@naver.com>
2024-03-31 09:48:41 +0900

    [BOJ]고양이/브론즈3/https://www.acmicpc.net/problem/10171

---
+++ gyeongmin/Week_13/BOJ_240331_고양이.java
@@ -0,0 +1,10 @@
+package gyeongmin.Week_13;
+
+public class BOJ_240331_고양이 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
