4b949e3c927deed1bd24b481ac9275bf6bc9d994
박경민 <pecobee813@naver.com>
2024-03-31 09:42:08 +0900

    [BOJ]A*B/브론즈5/https://www.acmicpc.net/problem/10998

---
+++ gyeongmin/Week_13/BOJ_240330_A_multiplication_B.java
@@ -0,0 +1,15 @@
+package gyeongmin.Week_13;
+
+import java.util.Scanner;
+
+public class BOJ_240330_A_multiplication_B {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.println(a * b);
+    }
+}
