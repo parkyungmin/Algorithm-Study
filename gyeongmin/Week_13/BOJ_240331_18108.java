7d355c6115a0509346a610ab3d9ece5e05c28f7a
박경민 <pecobee813@naver.com>
2024-03-31 09:43:13 +0900

    [BOJ]1998년생인 내가  태국에서는 2541년생?!/브론즈5/https://https://www.acmicpc.net/problem/18108

---
+++ gyeongmin/Week_13/BOJ_240331_18108.java
@@ -0,0 +1,14 @@
+package gyeongmin.Week_13;
+
+import java.util.Scanner;
+
+public class BOJ_240331_18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int y;
+        y = sc.nextInt();
+
+        System.out.println(y - 543);
+    }
+}
