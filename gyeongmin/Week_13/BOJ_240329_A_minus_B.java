6e3e90e2efe7fc5c34339e3bfd9b9756b71073a5
박경민 <pecobee813@naver.com>
2024-03-31 09:39:37 +0900

    [BOJ]A-B/브론즈5/https://www.acmicpc.net/problem/1001

---
+++ gyeongmin/Week_13/BOJ_240329_A_minus_B.java
@@ -0,0 +1,15 @@
+package gyeongmin.Week_13;
+
+import java.util.Scanner;
+
+public class BOJ_240329_A_minus_B {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A,B;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        System.out.println(A-B);
+    }
+}
