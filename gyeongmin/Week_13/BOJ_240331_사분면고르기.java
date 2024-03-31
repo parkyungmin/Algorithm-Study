f581c9ef848a29778b791189d1253b77d45ac2e3
박경민 <pecobee813@naver.com>
2024-03-31 09:50:40 +0900

    [BOJ]사분면고르기/브론즈5/https://www.acmicpc.net/problem/14681

---
+++ gyeongmin/Week_13/BOJ_240331_사분면고르기.java
@@ -0,0 +1,23 @@
+package gyeongmin.Week_13;
+
+import java.util.Scanner;
+
+public class BOJ_240331_사분면고르기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a,b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        if (a > 0 && b > 0)
+            System.out.println(1);
+        else if (a < 0 && b > 0)
+            System.out.println(2);
+        else if (a < 0 && b < 0)
+            System.out.println(3);
+        else if (a > 0 && b < 0)
+            System.out.println(4);
+    }
+}
