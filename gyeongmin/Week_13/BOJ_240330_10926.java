21cc59e62f9247ecb00a2959f954b224050e0234
박경민 <pecobee813@naver.com>
2024-03-31 09:42:33 +0900

    [BOJ]??!/브론즈5/https://www.acmicpc.net/problem/10926

---
+++ gyeongmin/Week_13/BOJ_240330_10926.java
@@ -0,0 +1,16 @@
+package gyeongmin.Week_13;
+
+import java.util.Scanner;
+
+public class BOJ_240330_10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = "??!";
+        String b;
+
+        b = sc.nextLine();
+
+        System.out.println(b + a);
+
+    }
+}
