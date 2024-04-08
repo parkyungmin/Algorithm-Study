9f25d7cb419956c4caa7efd26a7efabb5d91b543
박경민 <pecobee813@naver.com>
2024-04-08 18:52:24 +0900

    [BOJ]A+B-8/브론즈5/https://www.acmicpc.net/problem/11022

---
+++ gyeongmin/Week_14/BOJ_240408_A_plus_B_8.java
@@ -0,0 +1,22 @@
+package gyeongmin.Week_14;
+
+import java.util.Scanner;
+
+public class BOJ_240408_A_plus_B_8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for(int i=1; i <=T; i++){
+           int A = sc.nextInt();
+           int B = sc.nextInt();
+
+           System.out.println("Case #" + i + ": " + A + " + " + B +  " = " + (A+B));
+
+        }
+    }
+}
+
+
+
