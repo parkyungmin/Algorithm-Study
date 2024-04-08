5648d275b37037ff07cd307e738e4f552250adb4
박경민 <pecobee813@naver.com>
2024-04-08 19:17:20 +0900

    [BOJ]별찍기2/브론즈4/https://www.acmicpc.net/problem/2439

---
+++ gyeongmin/Week_14/BOJ_240408_별찍기2.java
@@ -0,0 +1,20 @@
+package gyeongmin.Week_14;
+
+import java.util.Scanner;
+
+public class BOJ_240408_별찍기2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=(N-i); j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
