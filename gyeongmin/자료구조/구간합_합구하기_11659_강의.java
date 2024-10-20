32bbc222a3f7b021ddb7bbdb64c8b57441c2787a
박경민 <pecobee813@naver.com>
2024-10-20 18:52:09 +0900

    백준 자바 문제풀이

---
+++ gyeongmin/자료구조/구간합_합구하기_11659_강의.java
@@ -0,0 +1,37 @@
+package gyeongmin.자료구조;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 구간합_합구하기_11659_강의 {
+    public static void main(String[] args) throws IOException {
+        //입력받는 수가 많을 때는 BufferReader를 쓴다.
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        //계산해야 하는 개수가 최대 10만개 까지 들어오는데, int형으로 받기 힘들기 때문에 Stringtoken을 사용한다.
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine()); //readLine() : 입력된 첫 번째 줄을 읽어옴.
+
+        //첫 번째 줄에 셋팅하는 입력값 셋팅
+        int suNo = Integer.parseInt(stringTokenizer.nextToken()); //정수형으로 변경
+        int quizNo = Integer.parseInt(stringTokenizer.nextToken());
+
+        //합배열 구하기
+        long[] S = new long[suNo + 1]; //+1은 인덱스 0번을 무시하고 사용하고 싶어서..
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine()); //한 줄에 입력된 값 한 번에 가져오기
+        for (int i=1; i<=suNo; i++) {
+            S[i] = S[i-1] + Integer.parseInt(stringTokenizer.nextToken()); //합배열 구하는 공식
+        }
+
+        //출력값 구하기
+        for(int q=0; q<quizNo; q++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine()); //한 줄에 입력된 값 한 번에 가져오기
+            int i = Integer.parseInt(stringTokenizer.nextToken());
+            int j = Integer.parseInt(stringTokenizer.nextToken());
+            System.out.println(S[j] - S[i-1]);
+
+        }
+
+    }
+}
---
+++ gyeongmin/자료구조/투포인터_연속된자연수의합구하기_2018_강의.java
@@ -0,0 +1,39 @@
+package gyeongmin.자료구조;
+
+import java.util.Scanner;
+
+public class 투포인터_연속된자연수의합구하기_2018_강의 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        //변수 초기화
+        int count = 1; //자기 자신은 미리 count
+        int start_index = 1;
+        int end_index = 1;
+        int sum = 1;
+        
+        //자기자신을 만나면 반복문 종료
+        while (end_index != N) {
+            //sum이 N과 같으면  count +1, end를 +1 해주고 sum에 더한다.
+            if(sum == N) {
+                count++;
+                end_index++;
+                sum += end_index;
+            }
+            //sum이 N보다 크면 sum에서 start를 빼주고
+            else if (sum > N) {
+                sum -= start_index;
+                start_index++;
+            }
+            //sum이 N보다 작으면 end +1, sum에 end값 더해준다.
+            else {
+                end_index++;
+                sum += end_index;
+            }
+        }
+
+        System.out.println(count);
+
+    }
+}
