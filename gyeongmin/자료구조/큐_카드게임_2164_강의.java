a4d0f6220c3267c155e76b3a410b1d9c38c4c4f3
박경민 <pecobee813@naver.com>
2024-10-21 00:58:35 +0900

    백준 자바 문제풀이

---
+++ gyeongmin/자료구조/큐_카드게임_2164_강의.java
@@ -0,0 +1,27 @@
+package gyeongmin.자료구조;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class 큐_카드게임_2164_강의 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        //큐 셋팅
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=1; i<=N; i++) {
+            queue.add(i);
+        }
+
+        //큐에 카드가 한 장 남을때까지 반복
+        while (queue.size() > 1) {
+            queue.poll(); //카드버리기
+            int temp = queue.poll();
+            queue.add(temp);
+        }
+
+        System.out.println(queue.poll());
+    }
+}
